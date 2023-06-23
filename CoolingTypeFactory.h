c5e35d38529d49e3a7181e36ca9ff28e53e21607
Sneha13145 <137185419+Sneha13145@users.noreply.github.com>
2023-06-23 10:50:11 +0530

    Update CoolingTypeFactory.h

--- CoolingTypeFactory.h
+++ CoolingTypeFactory.h
@@ -1,10 +1,1 @@
-#include "ICoolingType.h"
-#include "typewise-alert.h"
 
-class CoolingTypeFactory
-{
-  public:
-     CoolingTypeFactory();
-     ~CoolingTypeFactory();
-     static ICoolingType* getCoolingType(CoolingType coolingType);
-}
