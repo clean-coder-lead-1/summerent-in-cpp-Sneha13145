a26d67c4deb49b87e9587ae7454bc46574b84b44
Sneha13145 <137185419+Sneha13145@users.noreply.github.com>
2023-06-23 10:40:21 +0530

    Update PassiveCooling.cpp

--- PassiveCooling.cpp
+++ PassiveCooling.cpp
@@ -1,6 +1,15 @@
 #include "PassiveCooling.h"
 
-PassiveCooling()
+PassiveCooling::PassiveCooling()
 {
+}
 
+PassiveCooling::~PassiveCooling()
+{
+}
+
+void PassiveCooling::getTemperatureLimits(double& lowerLimit, double& upperLimit)
+{
+  lowerLimit = 0;
+  upperLimit = 35;
 }
